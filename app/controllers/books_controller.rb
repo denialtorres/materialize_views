7c5e0be6eb7bc1a6fbd6467c42a9a740b3f42634
Daniel Torres <denial.torres@gmail.com>
2024-03-09 16:25:51 -0600

    include a search form for a separate field

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
 
   # GET /books or /books.json
   def index
-    @books = BookStore.search(params[:q])
+    @books = BookStore.search(params)
                  .page(params[:page])
                  .without_count
                  .per(8)
--- app/models/book_store.rb
+++ app/models/book_store.rb
@@ -5,18 +5,20 @@ class BookStore < ApplicationRecord
     true
   end
 
-  include PgSearch::Model
-  pg_search_scope :pg_search,
-    against: [
-      :title,
-      :author_name,
-      :publisher_name,
-      :genre_name
-    ],
-    using: {
-      tsearch: { dictionary: 'english' }
-    }
+  # with materialized view
+  # include PgSearch::Model
+  # pg_search_scope :pg_search,
+  #   against: [
+  #     :title,
+  #     :author_name,
+  #     :publisher_name,
+  #     :genre_name
+  #   ],
+  #   using: {
+  #     tsearch: { dictionary: 'english' }
+  #   }
 
+  # with associations
   # pg_search_scope :pg_search,
   #                 against: :title,
   #                 using: {
@@ -28,12 +30,15 @@ class BookStore < ApplicationRecord
   #                   genre: :name
   #                 }
 
+  def self.search(params)
+    return all unless params.present?
 
+    where("title ILIKE ?", "%#{params[:title]}%")
+    .where("author_name ILIKE ?", "%#{params[:author_name]}%")
+    .where("publisher_name ILIKE ?", "%#{params[:publisher_name]}%")
+    .where("genre_name ILIKE ?", "%#{params[:genre_name]}%")
 
-  def self.search(query)
-    return all unless query.present?
-
-    pg_search(query)
+    # pg_search(query)
 
     # where("to_tsvector('english', title) @@ plainto_tsquery('english', :q)", q: query)
     #       .joins(:author).or(Book.where("authors.name ILIKE ?", "%#{query}%"))
